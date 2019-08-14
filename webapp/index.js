6c716d57b681041c6346747013c3da709cc23b31
Christian Rühl <ruehl@objectix.de>
2019-08-14 15:42:37 +0200

    Step 3

--- webapp/index.js
+++ webapp/index.js
@@ -1,12 +1,11 @@
 sap.ui.define([
-    "sap/m/Button",
-    "sap/m/MessageToast"
-], function (Button, MessageToast) {
-    var btn = new Button({
-        text: "Ready...!",
-        press: function () {
-            MessageToast.show("Hello World!!!");
-        }
-    });
-    btn.placeAt("content");
+	"sap/m/Text"
+
+], function (Text) {
+	"use strict";
+
+	new Text({
+		text: "Hello World"
+	}).placeAt("content");
+
 });
