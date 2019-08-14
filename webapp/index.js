0d2871f19072e3daf3877a04984c4b5df3fd337b
Christian Rühl <ruehl@objectix.de>
2019-08-14 15:37:57 +0200

    Removed TypeScript support

--- webapp/index.js
+++ webapp/index.js
@@ -1,16 +1,11 @@
 sap.ui.define([
     "sap/m/Button",
     "sap/m/MessageToast"
-], 
-/**
- * @param {typeof sap.m.Button} Button
- * @param {typeof sap.m.MessageToast} MessageToast
- * */
-function (Button, MessageToast) {
+], function (Button, MessageToast) {
     var btn = new Button({
-        text: "Ready...",
+        text: "Ready...!",
         press: function () {
-            MessageToast.show("Hello World!");
+            MessageToast.show("Hello World!!!");
         }
     });
     btn.placeAt("content");
