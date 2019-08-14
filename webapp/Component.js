3589f9033ea55ba1c269616e95e7a4c0cc3a1676
Christian Rühl <ruehl@objectix.de>
2019-08-14 16:45:24 +0200

    Step 10

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,17 +1,11 @@
 sap.ui.define([
     "sap/ui/core/UIComponent",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel"
-], function (UIComponent, JSONModel, ResourceModel) {
+    "sap/ui/model/json/JSONModel"
+], function (UIComponent, JSONModel) {
     "use strict";
     return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
         metadata: {
-            rootView: {
-                "viewName": "sap.ui.demo.walkthrough.view.App",
-                "type": "XML",
-                "async": true,
-                "id": "app"
-            }
+            manifest: "json"
         },
         init: function () {
             UIComponent.prototype.init.apply(this, arguments);
@@ -23,11 +17,6 @@ sap.ui.define([
             };
             var oModel = new JSONModel(oData);
             this.setModel(oModel);
-
-            var i18nModel = new ResourceModel({
-                bundleName: "sap.ui.demo.walkthrough.i18n.i18n"
-            });
-            this.setModel(i18nModel, "i18n");
         }
     });
 });
