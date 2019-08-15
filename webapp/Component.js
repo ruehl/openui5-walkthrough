910d6bcc9174150c38b0c147dcd8b77a31862443
Christian Rühl <ruehl@objectix.de>
2019-08-15 17:03:02 +0200

    Step 31

--- webapp/Component.js
+++ webapp/Component.js
@@ -20,6 +20,7 @@ sap.ui.define([
             this.setModel(oModel);
 
             this._helloDialog = new HelloDialog(this.getRootControl());
+            this.getRouter().initialize();
         },
         exit : function() {
 			this._helloDialog.destroy();
